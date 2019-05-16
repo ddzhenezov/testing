3894da87649d265ba420d334cf062af8604dbfdc
Dimitar Dzhenezov <dddzhenezov@gmail.com>
2019-05-16 23:35:48 +0300

    class free table

---
+++ ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseArrayOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] texts = Console.ReadLine().Split();
+
+            for (var i = 0; i < texts.Length / 2; i++)
+            {
+                string firstText = texts[i];
+                texts[i] = texts[texts.Length - i - 1];
+                texts[texts.Length - i - 1] = firstText;
+            }
+            for (var i = texts.Length - 1; i >= 0; i--)
+            {
+                Console.Write(texts[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
